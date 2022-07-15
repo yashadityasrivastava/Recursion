ec2b98143f8d95c780bff19c06e9c74d7087de41
yashadityasrivastava <yashadityasrivastava@gmail.com>
2022-07-15 06:37:27 +0530

    Adding PrintName() method

--- src/Basic_Problems.java
+++ src/Basic_Problems.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Basic_Problems {
+import java.util.Scanner;
+
+public class Basic_Problems {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        PrintName(1,n);
+
+    }
+
+    public static void PrintName(int i,int n){
+        if(i > n )
+            return;
+        System.out.println("Yash");
+        PrintName(i+1,n);
+    }
+
 }
